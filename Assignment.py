5e595aced210add12ea68536a4b097f283fe1f80
Ong-ZiKai <ongzikai@gmail.com>
2023-12-08 13:09:31 +0800

    inplementation of main menu

--- Assignment.py
+++ Assignment.py
@@ -3,4 +3,20 @@ import random
 import os
 import sys
 
+#main menu function
+def show_main_menu():
+    print("1. Start new game")
+    print("2. Load saved game")
+    print("3. Quit")
+    print()
+    while True:
+        try:
+            option = int(input('Your choice? '))
+            break
+        except:
+            print('wrong input')
+            
+    return option
+
 #main software
+menu = show_main_menu()
